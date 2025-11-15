b76fd7f1316afc263a27308b336439de9fcdeea4
alexischang1 <alexisn1@uw.edu>
2025-11-14 19:34:21 -0800

    switched from rmarkdown::render() to knitr::purl() to avoid intermediate files and new folders with all code chunk outputs.

--- 00_master_run_all_figures.R
+++ 00_master_run_all_figures.R
@@ -1,32 +1,22 @@
 ### This RScript points to each figure-specific script to run sequentially with one line ### -----------
-
 ###  To run this code, in console within this Rproject, run: source("00_master_run_all_figures.R")
 
-library(rmarkdown)
 
-rmarkdown::render("01_Script_MainAndSuppFig1_NHS_vs_Halo_1mgInput_Triplicate.Rmd")
+source(knitr::purl("01_Script_MainAndSuppFig1_NHS_vs_Halo_1mgInput_Triplicate.Rmd"))
+source(knitr::purl("02_Script_MainFig2_VaryPeptideConcentration_HighpTyrContent.Rmd"))
+source(knitr::purl("03_Script_MainFig3to6_SuppFig9to15_ProteomeLevel.Rmd"))
+source(knitr::purl("04_Script_MainFig3to6_SuppFig9to15_GlobalPhosphoSiteLevel.Rmd"))
+source(knitr::purl("05_Script_MainFig3to6_SuppFig9to15_GlobalPhosphoIsoformLevel.Rmd"))
+source(knitr::purl("06_Script_MainFig3to6_SuppFig9to15_pTyrSiteLevel.Rmd"))
+source(knitr::purl("07_Script_MainFig3to6_SuppFig9to15_pTyrIsoformLevel.Rmd"))
+source(knitr::purl("08_Script_MainFig3to6_SuppFig9to15_Reddy2016_EGFRTMT_sectimecourse.Rmd"))
+source(knitr::purl("09_Script_SuppFig2_NHS_vs_Halo_VaryPeptideInput.Rmd"))
+source(knitr::purl("10_Script_SuppFig5_VaryBeadAmount.Rmd"))
+source(knitr::purl("11_Script_SuppFig6_VaryPeptideConcentration_LowpTyrContent.Rmd"))
+source(knitr::purl("12_Script_SuppFig7_EasyPositiveControlSample_Yeast_vSrc.Rmd"))
+source(knitr::purl("13_Script_SuppFig8_BeadAge_4months.Rmd"))
 
-rmarkdown::render("02_Script_MainFig2_VaryPeptideConcentration_HighpTyrContent")
 
-rmarkdown::render("03_Script_MainFig3to6_SuppFig9to15_ProteomeLevel")
 
-rmarkdown::render("04_Script_MainFig3to6_SuppFig9to15_GlobalPhosphoSiteLevel")
 
-rmarkdown::render("05_Script_MainFig3to6_SuppFig9to15_GlobalPhosphoIsoformLevel")
-
-rmarkdown::render("06_Script_MainFig3to6_SuppFig9to15_pTyrSiteLevel")
-
-rmarkdown::render("07_Script_MainFig3to6_SuppFig9to15_pTyrIsoformLevel")
-
-rmarkdown::render("08_Script_MainFig3to6_SuppFig9to15_Reddy2016_EGFRTMT_sectimecourse")
-
-rmarkdown::render("09_Script_SuppFig2_NHS_vs_Halo_VaryPeptideInput")
-
-rmarkdown::render("10_Script_SuppFig5_VaryBeadAmount")
-
-rmarkdown::render("11_Script_SuppFig6_VaryPeptideConcentration_LowpTyrContent")
-
-rmarkdown::render("12_Script_SuppFig7_EasyPositiveControlSample_Yeast_vSrc")
-
-rmarkdown::render("13_Script_SuppFig8_BeadAge_4months")
 
