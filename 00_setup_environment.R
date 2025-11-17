881861c40827d0eb0591e3dfe42eef42f6c85719
alexischang1 <alexisn1@uw.edu>
2025-11-16 16:35:01 -0800

    added conditional install of BiocManager and limma to setup script because renv X biocmanager incompatibilities mean they are not default installed.

--- 00_setup_environment.R
+++ 00_setup_environment.R
@@ -10,5 +10,31 @@ if (!requireNamespace("renv", quietly = TRUE)) {
   install.packages("renv", repos = "https://cran.r-project.org")
 }
 
+
+# ---------------------------------------------------------
+# Install BiocManager if missing
+# ---------------------------------------------------------
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+
+# ---------------------------------------------------------
+# Set the correct Bioconductor version for R 4.4.2
+# (R 4.4.x → Bioc 3.19)
+# ---------------------------------------------------------
+# BiocManager::install(version = "3.19")
+# 
+# # Ensure repos are set correctly (renv-safe)
+# options(repos = BiocManager::repositories(version = "3.19"))
+
+# ---------------------------------------------------------
+# Install limma if missing
+# ---------------------------------------------------------
+if (!requireNamespace("limma", quietly = TRUE)) {
+  BiocManager::install("limma")
+}
+
+# Load package
+library(limma)
 message("Setup complete. Now restart R in this project and run renv::restore().")
 
